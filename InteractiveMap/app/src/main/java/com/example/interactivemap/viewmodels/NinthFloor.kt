3ed0cf1d0c6c47f3c90212c53b627124f5367b3c
Strupervlg <53981215+Strupervlg@users.noreply.github.com>
2021-10-27 21:39:39 +0300

    Исправлены метки на 9 этаже

--- InteractiveMap/app/src/main/java/com/example/interactivemap/viewmodels/NinthFloor.kt
+++ InteractiveMap/app/src/main/java/com/example/interactivemap/viewmodels/NinthFloor.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -27,16 +25,16 @@ class NinthFloor(application: Application) : AndroidViewModel(application) {
     private val tileStreamProvider = makeTileStreamProvider9(appContext)
 
     private val markers = listOf(
-        MarkerInfo("901", 0.518, 0.62, R.drawable.cab901),
-        MarkerInfo("902а", 0.46, 0.62, R.drawable.cab902a),
-        MarkerInfo("902б", 0.4, 0.62, R.drawable.cab902b),
-        MarkerInfo("902в", 0.347, 0.62, R.drawable.cab902v),
+        MarkerInfo("901", 0.518, 0.59, R.drawable.cab901),
+        MarkerInfo("902а", 0.46, 0.59, R.drawable.cab902a),
+        MarkerInfo("902б", 0.4, 0.59, R.drawable.cab902b),
+        MarkerInfo("902в", 0.347, 0.59, R.drawable.cab902v),
         MarkerInfo("903", 0.3, 0.62, R.drawable.cab903),
-        MarkerInfo("904", 0.245, 0.62, R.drawable.cab904),
-        MarkerInfo("905", 0.245, 0.45, R.drawable.cab905),
-        MarkerInfo("906", 0.505, 0.45, R.drawable.cab906),
-        MarkerInfo("907", 0.535, 0.45, R.drawable.cab907),
-        MarkerInfo("908", 0.57, 0.41, R.drawable.cab908)
+        MarkerInfo("904", 0.245, 0.58, R.drawable.cab904),
+        MarkerInfo("905", 0.245, 0.41, R.drawable.cab905),
+        MarkerInfo("906", 0.505, 0.40, R.drawable.cab906),
+        MarkerInfo("907", 0.535, 0.40, R.drawable.cab907),
+        MarkerInfo("908", 0.57, 0.37, R.drawable.cab908)
     )
 
     val state: MapState by mutableStateOf(
@@ -46,7 +44,7 @@ class NinthFloor(application: Application) : AndroidViewModel(application) {
                     Icon(
                         painter = painterResource(id = marker.idPaint),
                         contentDescription = null,
-                        modifier = Modifier.size(50.dp),
+                        modifier = Modifier.size(35.dp),
                         tint = Color(0xCC2196F3)
                     )
                 }
